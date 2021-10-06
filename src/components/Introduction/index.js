30e98089e77de5326704e86d84e08cf5d06b98aa
Matheus Gomes <matheusgomes192@hotmail.com>
2021-10-06 16:13:06 -0300

    Criando folders + Component Introduction e começando sua estilização

---
+++ src/components/Introduction/index.js
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from 'react';
+
+import BackgroundVideo from '../../assets/videos/technology-background-2.mp4';
+
+import styles from './introduction.module.scss';
+
+export default function Introduction () {
+    const video = useRef();
+
+    useEffect(() => {
+        video.current.play()
+    }, []);
+
+    return(
+        <div className={styles.container}>
+            <video ref={video} src={BackgroundVideo} muted loop/>
+
+            <div className={styles.content}>
+                <p>
+                    Olá, meu nome é 
+                    <span className={styles.textColorize}> Matheus Gomes</span>
+                    <br /> 
+                    <span>Eu sou um desenvolvedor Full-Stack</span>
+                </p>
+            </div>
+        </div>
+    )
+}
