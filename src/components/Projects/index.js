e8c6b0cb098ccb89d2a4241f099e5e1c2f8c2d35
Matheus Gomes <matheusgomes192@hotmail.com>
2021-10-08 21:41:55 -0300

    Finalizando design do Modal que mostra os detalhes do projeto que o usuário clicou

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,25 +1,91 @@
 import React, { useState } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
-import Img from '../../assets/pizzaria-portfolio/1.png';
+import { BiLinkExternal } from 'react-icons/bi';
+
+import ImgPizzaria1 from '../../assets/pizzaria-portfolio/1.png';
+import ImgPizzaria2 from '../../assets/pizzaria-portfolio/2.png';
+import ImgPizzaria3 from '../../assets/pizzaria-portfolio/3.png';
+import ImgPizzaria4 from '../../assets/pizzaria-portfolio/4.png';
+import ImgPizzaria5 from '../../assets/pizzaria-portfolio/5.png';
 
 import styles from './projects.module.scss';
 import './animation.css';
 
 let projectsArray = [
-    {id: 1, img: Img, name: 'Pizza Home', languages: 'React / Node', languageFilter: 'react', languageFilter2: 'node', node: true},
-    {id: 2, img: Img, name: 'Recipe', languages: 'React-Native / Node', languageFilter: 'react-native', languageFilter2: 'node', node: true},
+    {id: 1, img: ImgPizzaria1, name: 'Pizza Home', description: 'Um lindo website de apresentação para uma Pizzaria', whatCanDo: 'Pizza Home é um website simples, com uma API feita em Node para testar como seria a visão do usuário ao fazer os pedidos', languages: 'React / Node', languageFilter: 'react', languageFilter2: 'node', node: true},
+    {id: 2, img: ImgPizzaria1, name: 'Recipe', languages: 'React-Native / Node', languageFilter: 'react-native', languageFilter2: 'node', node: true},
 ];
 
 export default function Projects ({ showTitle, showLine, showFilter }) {
     const [filter, setFilter] = useState('all');
     const [modalVisible, setModalVisible] = useState(false);
-    const [img, setImg] = useState();
+
+    const [img, setImg] = useState('');
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [whatCanDo, setWhatCanDo] = useState('');
+
+    const [hover, setHover] = useState(false);
+
+    const arrowPrev = (clickHandler) => {
+        return(
+            <div onClick={clickHandler} style={{left: 0}} className={styles.arrow}>
+                <span>&#x276E;</span>
+            </div>
+        )
+    }
+
+    const arrowNext = (clickHandler) => {
+        return(
+            <div onClick={clickHandler} style={{right: 0}} className={styles.arrow}>
+                <span>&#x276F;</span>
+            </div>
+        )
+    }
 
     const Modal = () => {
         return(
             <div className={styles.modalContainer}>
-                <div onClick={() => setModalVisible(false)} className={styles.modal}>
-                    <img src={img} />
+                <div className={styles.modal}>
+                    <Carousel renderArrowNext={arrowNext} renderArrowPrev={arrowPrev} useKeyboardArrows infiniteLoop showStatus={false} showThumbs={false} showIndicators={false} >
+                        <div>
+                            <img src={ImgPizzaria1} />
+                        </div>
+
+                        <div>
+                            <img src={ImgPizzaria2} />
+                        </div>
+
+                        <div>
+                            <img src={ImgPizzaria3} />
+                        </div>
+
+                        <div>
+                            <img src={ImgPizzaria4} />
+                        </div>
+
+                        <div>
+                            <img src={ImgPizzaria5} />
+                        </div>
+                    </Carousel>
+
+                    <div className={styles.headerModal}>
+                        <h2>{name}</h2>
+                        <span>{description}</span>
+                    </div>
+
+                    <p>{whatCanDo}</p>
+
+                    <div className={styles.buttonModal}>
+                        <BiLinkExternal className={styles.icon} size={18} />
+                        <span>Ver site</span>
+                    </div>
+
+                    <div onClick={() => setModalVisible(false)} className={styles.closeButtonModal}>
+                        <span>&#x2717;</span>
+                    </div>
                 </div>
             </div>
         )
@@ -34,7 +100,7 @@ export default function Projects ({ showTitle, showLine, showFilter }) {
                         <span>{item.languages}</span>
                     </div>
 
-                    <div onClick={() => openModal(item.img)} className={styles.projectsItemButton}>
+                    <div onClick={() => openModal(item.img, item.name, item.description, item.whatCanDo)} className={styles.projectsItemButton}>
                         <span>Saiba Mais</span>
                     </div>
                 </div>
@@ -42,8 +108,11 @@ export default function Projects ({ showTitle, showLine, showFilter }) {
         )
     }
 
-    function openModal (img) {
+    function openModal (img, name, description, whatCanDo) {
         setImg(img);
+        setName(name);
+        setDescription(description);
+        setWhatCanDo(whatCanDo);
         setModalVisible(true);
     }
 
@@ -93,6 +162,7 @@ export default function Projects ({ showTitle, showLine, showFilter }) {
                             <RenderItem item={item} k={k} />
                     ))}
                 </div>
+
             </section>
         </div>
     )
