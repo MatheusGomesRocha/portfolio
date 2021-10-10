246f42ac5773f4faa7c663f7c7779a43697c3145
Matheus Gomes <matheusgomes192@hotmail.com>
2021-10-10 10:43:28 -0300

    Criando component Header

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 
+import Header from "../Header";
 import HeaderAnimated from "../HeaderAnimated";
+
 import styles from './about.module.scss';
 import './animation.css';
 
@@ -21,54 +23,58 @@ let languagesArray = [
 
 export default function About ({showTitle, showLine, showContent}) {
     return(
-        <div id="about" className={styles.container}>
-            <HeaderAnimated title="Sobre" direction='left' showTitle={showTitle} showLine={showLine} />
+        <div>
+            <Header />
 
-            {/* <div className={styles.goalArea}>
-                <div className={showGoal ? styles.goalItem : styles.goalHiddenItem}>
-                    <div className={styles.goalPolygon}></div>
+            <div id="about" className={styles.container}>
+                <HeaderAnimated title="Sobre" direction='left' showTitle={showTitle} showLine={showLine} />
 
-                    <h2>Backend</h2>
-                </div>
+                {/* <div className={styles.goalArea}>
+                    <div className={showGoal ? styles.goalItem : styles.goalHiddenItem}>
+                        <div className={styles.goalPolygon}></div>
 
-                <div className={showGoal ? styles.goalItem : styles.goalHiddenItem}>
-                    <div style={{animationDelay: '250ms'}} className={styles.goalPolygon}></div>
+                        <h2>Backend</h2>
+                    </div>
 
-                    <h2>Frontend</h2>
-                </div>
+                    <div className={showGoal ? styles.goalItem : styles.goalHiddenItem}>
+                        <div style={{animationDelay: '250ms'}} className={styles.goalPolygon}></div>
 
-                <div className={showGoal ? styles.goalItem : styles.goalHiddenItem}>
-                    <div style={{animationDelay: '500ms'}} className={styles.goalPolygon}></div>
+                        <h2>Frontend</h2>
+                    </div>
 
-                    <h2>Banco de Dados</h2>
-                </div>
-            </div> */}
+                    <div className={showGoal ? styles.goalItem : styles.goalHiddenItem}>
+                        <div style={{animationDelay: '500ms'}} className={styles.goalPolygon}></div>
 
-            <section className={styles.content}>
-                <div id={showContent ? 'showContentLeft' : 'hideContentLeft'} className={styles.left}>
-                    <div className={styles.image} />
+                        <h2>Banco de Dados</h2>
+                    </div>
+                </div> */}
 
-                    <h2>Quem é esse cara?</h2>
+                <section className={styles.content}>
+                    <div id={showContent ? 'showContentLeft' : 'hideContentLeft'} className={styles.left}>
+                        <div className={styles.image} />
 
-                    <p>
-                        Eu sou um Desenvolvedor Full-Stack. Comecei nessa área à quase 3 anos e desde então venho focando em me aperfeiçoar como um Desenvolvedor. Usar a lógica para resolver problemas do Backend e Banco de Dados é minha paixão. 
-                        Adoro recriar designs para melhorar meu código no Frontend.
-                    </p>
-                </div>
+                        <h2>Quem é esse cara?</h2>
 
-                <div id={showContent ? 'showContentRight' : 'hideContentRight'} className={styles.right}>
-                    {languagesArray.map((item, k) => (
-                        <div key={k} className={styles.progressBar}>
-                            <div style={{width: `${item.value}%`}} className={styles.progressActive}></div>
-                            <div className={styles.progressNameArea}>
-                                <span>{item.name}</span>
+                        <p>
+                            Eu sou um Desenvolvedor Full-Stack. Comecei nessa área à quase 3 anos e desde então venho focando em me aperfeiçoar como um Desenvolvedor. Usar a lógica para resolver problemas do Backend e Banco de Dados é minha paixão. 
+                            Adoro recriar designs para melhorar meu código no Frontend.
+                        </p>
+                    </div>
+
+                    <div id={showContent ? 'showContentRight' : 'hideContentRight'} className={styles.right}>
+                        {languagesArray.map((item, k) => (
+                            <div key={k} className={styles.progressBar}>
+                                <div style={{width: `${item.value}%`}} className={styles.progressActive}></div>
+                                <div className={styles.progressNameArea}>
+                                    <span>{item.name}</span>
+                                </div>
+
+                                <span className={styles.progressValue}>{item.value}%</span>
                             </div>
-
-                            <span className={styles.progressValue}>{item.value}%</span>
-                        </div>
-                    ))}
-                </div>
-            </section>
+                        ))}
+                    </div>
+                </section>
+            </div>
         </div>
     )
 }
